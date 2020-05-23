f2837f323745d40adb1ca8c28d591c36f7fbbcf1
AnayaJohn <64412959+AnayaJohn@users.noreply.github.com>
2020-05-23 20:31:32 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -14,7 +14,13 @@ function setup() {
   block3= new Block(390,235,30,40);
   block4= new Block(420,235,30,40);
   block5= new Block(450,235,30,40);
-}
+
+  block6= new Block(360,195,30,40);
+  block7= new Block(390,195,30,40);
+  block8= new Block(420,195,30,40);
+
+  block9= new Block(390,155,30,40);
+  }
 
 function draw() {
   background(0); 
@@ -23,6 +29,12 @@ function draw() {
   block3.display();
   block4.display();
   block5.display();
+
+  block6.display();
+  block7.display();
+  block8.display();
+
+  block9.display();
 
   ground.display();
   drawSprites()
